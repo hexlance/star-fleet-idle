8006f9fa2e64ac4cb9a75800ace2508d4a7a88ad
chenxander <chenxander23@gmail.com>
2021-01-07 12:56:26 -0500

    update stuff

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
 var saveGame = localStorage.getItem('gameSave');
 var gameData = {
+    // fleet
+    corvettes: 1,
+    frigates: 0,
+    destroyers: 0,
+    cruisers: 0,
+    battleships: 0,
+    dreadnoughts: 0,
+    motherships: 0,
+
+    // resources
     credits: 0,
     iron: 0,
     nanofiber: 0,
     antimatter: 0,
     hydrogen: 0,
     researchPts: 0,
+
+    // rates
     creditsRate: 1,
     ironRate: 0,
     nanofiberRate: 0,
@@ -24,22 +36,26 @@ var mainGameLoop = window.setInterval(function() {
     gameData.lastTick = Date.now()
 
     gameData.credits += Math.round(gameData.creditsRate * (diff / 1000))
-    update("credits", gameData.credits + " Credits")
+    update("credits", gameData.credits)
     gameData.iron += Math.round(gameData.ironRate * (diff / 1000))
-    update("iron", gameData.iron + " Iron")
+    update("iron", gameData.iron)
     gameData.nanofiber += Math.round(gameData.nanofiberRate * (diff / 1000))
-    update("nanofiber", gameData.nanofiber + " Nanofiber")
+    update("nanofiber", gameData.nanofiber)
     gameData.antimatter += Math.round(gameData.antimatterRate * (diff / 1000))
-    update("antimatter", gameData.antimatter + " Antimatter")
+    update("antimatter", gameData.antimatter)
     gameData.hydrogen += Math.round(gameData.hydrogenRate * (diff / 1000))
-    update("hydrogen", gameData.hydrogen + " Hydrogen")
+    update("hydrogen", gameData.hydrogen)
     gameData.researchPts += Math.round(gameData.researchPtsRate * (diff / 1000))
-    update("research", gameData.researchPts + " Research Points")
+    update("research", gameData.researchPts)
 
 }, 1000)
 
-var saveGameLoop = window.setInterval(function() {
+function saveGame() {
     localStorage.setItem('gameSave', JSON.stringify(gameData))
+}
+
+var saveGameLoop = window.setInterval(function() {
+    saveGame();
 }, 15000)
 
 function tab(tab) {
